3f2dd77bba69db3ef58cbd80083f7577962e73d6
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-09-25 09:54:58 +0700

    first commit

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,196 @@
+import React, {useState, useEffect} from "react";
+import {
+  Input,
+  Image,
+  Row,
+  Col,
+  Button,
+  Space,
+  Checkbox,
+  Radio,
+  Select,
+  Typography,
+} from "antd";
+import { createAPIEndpoint, ENDPIONTS } from "../../api";
+import Timer from "../../component/Timer";
+
+const { Title, Text } = Typography;
+const style = {
+  background: "#5fb082",
+  padding: "8px 0",
+  height: "70vh",
+};
+
+
+export default function Dashboard() {
+    
+    let selectedWinrate = localStorage.getItem("winrate")
+    console.log(selectedWinrate)
+    //const [deposit, setDeposit] = useState()
+    const [winrate] = useState(selectedWinrate)
+    const [depoValue, setDepoValue] = useState("")
+
+    
+    // switch(winrate) {
+    //     case "68.9%":
+          
+    //     setDeposit("008")
+    //     console.log(deposit)  
+    //     break;
+    //     case "96.9%": setDeposit("006")
+    //     console.log("deposit",deposit)  
+    //     break;
+    //     case "97.3%": setDeposit("003")
+    //     console.log("deposit",deposit)  
+    //     break;
+    //     case "99.9%": setDeposit("009")
+    //     console.log("deposit",deposit)  
+    //     break;  
+    //     default: console.log(null)
+    // }
+     let deposit = "";
+    if (winrate === "68.9%") {
+        deposit = "006"
+    }
+    if (winrate === "96.9%") {
+        deposit = "006"
+    }
+    if (winrate === "97.3%") {
+        deposit = "006"
+    }
+    if (winrate === "99.9%") {
+        deposit = "006"
+    }
+    // if(selectedWinrate === "68.9%")
+    // {
+    //     setDeposit()
+    // }
+ 
+
+    // useEffect(() => {
+    //     createAPIEndpoint(ENDPIONTS.FOODITEM).fetchAll()
+    //         .then(res => {
+    //             setDepoValue(res.data);
+    //             setDepoValue(res.data);
+    //         })
+    //         .catch(err => console.log(err))
+    // }, [])
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#12291c",
+          paddingBottom: 30,
+          paddingLeft: 50,
+          paddingRight: 50,
+        }}
+      >
+        <div
+          style={{
+            minHeight: "30vh",
+            backgroundColor: "#0d120e",
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "50px",
+          }}
+        >
+          <Title style={{ color: "#fff" }}>Slot Hacked</Title>
+          <Image
+            style={{ height: "200px", width: "200px" }}
+            preview={false}
+            src="https://img1.pngdownload.id/20180324/fzq/kisspng-2013-singapore-cyberattacks-anonymous-security-hac-anonymous-mask-5ab5d1467e8178.4964785115218650305182.jpg"
+          />
+        </div>
+        <Row gutter={16}>
+          <Col className="gutter-row" span={4}>
+            <div style={style}>
+              <div
+                style={{
+                  height: 200,
+                  backgroundColor: "ThreeDLightShadow",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Title level={4}>EXPIRED CHEAT TANGGAL:</Title>
+                <Title level={4} style={{ color: "white" }}>
+                25 Januari 2025
+                </Title>
+              </div>
+              <div
+                style={{
+                  marginTop: 50,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Title level={4}>TIME</Title>
+                <Title level={4} style={{ color: "white" }}>
+                  
+                  <Timer minutes={10} seconds={10} />
+                </Title>
+              </div>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={16}>
+            <div
+              style={{
+                ...style,
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Title level={2}>INFO PENTING!</Title>
+              <Title level={4}>Win Rate {winrate} MAXWIN</Title>
+              <Title level={5}>Berlaku untuk ALL Games Slot</Title>
+              <Title level={5}>
+                Untuk konfirmasi aktivasi Cheat Slot dengan Winrate 100%!
+              </Title>
+              <Title level={5}>WAJIB DEPOSIT</Title>
+              <Text style={{color: "#fff", fontWeight: 600}}>Rp.100.{deposit}</Text>
+              <Text style={{color: "#fff", fontWeight: 600}}>RP.200.{deposit}</Text>
+              <Text style={{color: "#fff", fontWeight: 600}}>Rp.500.{deposit}</Text>
+              <Text style={{color: "#fff", fontWeight: 600}}>Rp.400.{deposit}</Text>
+              <Text style={{color: "#fff", fontWeight: 600}}>RP.500.{deposit}</Text>
+              <Text style={{color: "#fff", fontWeight: 600}}>RP.600.{deposit}</Text>
+              <Title level={3}>Win Rate  {winrate} MAXWIN</Title>
+              <Title level={3}>
+                SEMAKIN TINGGI ANGKA DEPOSIT SEMAKIN TINGGI KEMENANGAN YANG DI
+                DAPAT
+              </Title>
+            </div>
+          </Col>
+
+          <Col className="gutter-row" span={4}>
+            <div
+              style={{
+                backgroundColor: "#67f50a",
+                height: "70vh",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Title level={3}>JANGAN TUTUP</Title>
+              <Title level={3}>CHEAT</Title>
+              <Title level={3}>ENGINE SLOT</Title>
+              <Title level={3}>SAAT BERMAIN</Title>
+              <Title level={3}>AGAR</Title>
+              <Title level={3}>CHEAT</Title>
+              <Title level={3}>TETAP AKTIF</Title>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import {
+  Input,
+  Image,
+  Row,
+  Col,
+  Button,
+  Space,
+  Checkbox,
+  Radio,
+  Select,
+  Typography,
+} from "antd";
+import { Link } from "react-router-dom";
+
+const { Option } = Select;
+const { Title } = Typography;
+
+const style = {
+  background: "#5fb082",
+  padding: "8px 0",
+  height: "70vh",
+  paddingLeft: 120,
+  paddingTop: 70,
+};
+const handleWinrate = (value) => {
+  localStorage.setItem("winrate",value)
+ // console.log(e)
+}
+
+function Home() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#12291c",
+          paddingBottom: 30,
+        }}
+      >
+        <div
+          style={{
+            minHeight: "30vh",
+            backgroundColor: "#0d120e",
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "50px",
+          }}
+        >
+          <Title style={{ color: "#fff" }}>Welcomet to Cheat Engine</Title>
+          <Image
+            style={{ height: "200px", width: "200px" }}
+            preview={false}
+            src="https://img1.pngdownload.id/20180324/fzq/kisspng-2013-singapore-cyberattacks-anonymous-security-hac-anonymous-mask-5ab5d1467e8178.4964785115218650305182.jpg"
+          />
+        </div>
+        <Row gutter={16}>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>
+              <Title level={2}>Cheat Engine</Title>
+              <Checkbox.Group style={{ width: "100%" }} onChange="">
+                <Row>
+                  <Col span={24}>
+                    <Checkbox value="A">Auto Scatter</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="Auto WILD">Auto WILD</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="Maxwin">Maxwin</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="Sensational">Sensational</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="Auto Win">Auto Win</Checkbox>
+                  </Col>
+                </Row>
+              </Checkbox.Group>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>
+              <Title level={2}>Provider Slot</Title>
+              <Radio.Group onChange="" value="">
+                <Space direction="vertical">
+                  <Radio value={1}>Pragmatic Play</Radio>
+                  <Radio value={2}>Habanero</Radio>
+                  <Radio value={3}>Spagegaming</Radio>
+                  <Radio value={3}>PG Soft</Radio>
+                  <Radio value={3}>CQ9</Radio>
+                  <Radio value={3}>Joker</Radio>
+                  <Radio value={3}>iSoftbet</Radio>
+                  <Radio value={3}>PlayTech</Radio>
+                </Space>
+              </Radio.Group>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>
+              <Title level={2}>Best Fitur</Title>
+              <Space direction="vertical">
+                <Select
+                  defaultValue="Auto Winrate 68,9%"
+                  style={{ width: 200 }}
+                  onChange={handleWinrate}
+                >
+                  <Option value="68,9%">Auto Winrate 68,9%</Option>
+                  <Option value="96,9%">Auto Winrate 86,9%</Option>
+                  <Option value="97,3%">Auto Winrate 97,3%</Option>
+                  <Option value="99,9%">Auto Winrate 99,9%</Option>
+                </Select>
+                <Select
+                  defaultValue="Auto Spin"
+                  style={{ width: 200 }}
+                  onChange=""
+                >
+                  <Option value="Auto Spin">Auto Spin</Option>
+                  <Option value="Spin Cepat">Spin Cepat</Option>
+                  <Option value="Spin Turbo">Spin Turbo</Option>
+                  <Option value="Buy Spin">Buy Spin</Option>
+                  <Option value="Normal Spin">Normal Spin</Option>
+                  <Option value="Taruhan Ganda">Taruhan Ganda</Option>
+                </Select>
+              </Space>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>
+              <Title level={2}>Hack Now!</Title>
+              <Space direction="vertical">
+                <Input placeholder="Username" />
+                <Link to="dashboard">
+                  <Button type="primary" block danger href="Dashboard">
+                    Hacked
+                  </Button>
+                </Link>
+              </Space>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Home;
